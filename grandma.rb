d8b0dcc1ce3eb18f559ea67edebdfae0d636160a
Pedro Huertas <pmh242@gmail.com>
2019-11-08 06:41:02 +0000

    Done.

--- grandma.rb
+++ grandma.rb
@@ -2,12 +2,17 @@
 
 
 def speak_to_grandma(words)
-  if words == "I LOVE YOU GRANDMA!"
+  
+  if "I LOVE YOU TOO PUMPKIN!" == "I LOVE YOU GRANDMA!"
     words = "I LOVE YOU TOO PUMPKIN!"
+    
   elsif words.upcase == words
     words = "NO, NOT SINCE 1938!"
+    
   else words = 'HUH?! SPEAK UP, SONNY!'
+    
   end
+  
   words
 end
 
